6888f14a176e815e8db8281e9e6e3f1f487a6f3e
Jasper Katalevsky <jyk003@bucknell.edu>
2025-12-17 16:58:34 -0700

    Documented LoginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,6 +3,11 @@ import { SignInForm } from "../components/auth/SignInForm";
 import { SignUpForm } from "../components/auth/SignUpForm";
 import { OtpForm } from "../components/auth/OtpForm"; 
 
+/**
+ * Creates the general login page
+ * 
+ * @returns whichever Login page is necessary in the form of a 'form' visible to the user
+ */
 export function LoginPage() {
     const [currentView, setCurrentView] = useState('otp'); 
     const forms = {
