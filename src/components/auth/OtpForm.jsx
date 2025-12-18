2892de3af6d2529c2aa110696f94bf9778ee929c
Jasper Katalevsky <jyk003@bucknell.edu>
2025-12-18 14:30:18 -0700

    Removed revealing error for absent email (not in db)

--- src/components/auth/OtpForm.jsx
+++ src/components/auth/OtpForm.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import { useAuth } from "../../hooks/UseAuth";
+import { useRef } from "react";
 
 export function OtpForm({setCurrentView}) {
     const { sendOtp, verifyOtp } = useAuth(); 
-    const [ emailValue, setEmailValue ] = useState(''); 
+    const [emailValue, setEmailValue ] = useState(''); 
     const [emailError, setEmailError] = useState('');  
+    const [submitButtonContent, setSubmitButtonContent] = useState('Send Code'); 
+    const [otp, setOtp] = useState(['', '', '', '', '', '']); 
+    const inputs = useRef([]); 
 
-    const isButtonDisabled = emailValue.length === 0 || emailError !== ''; 
+    const isButtonDisabled = emailValue.length === 0 || emailError !== '' || (submitButtonContent !== "Send Code" && hasBlankValues(otp)); 
+    const length = 6; 
 
 
     // Handles email text input change
@@ -45,25 +50,105 @@ export function OtpForm({setCurrentView}) {
     // Handles email submittion for email otp
     async function handleSubmitEmail() {
 
-        document.getElementById("otp-container").classList.remove("hidden"); 
+        if (submitButtonContent === "Send Code") {
+            // Reveal the area where we ask for the code to be entered
+            document.getElementById("otp-container").classList.remove("hidden"); 
+            setSubmitButtonContent("Log In"); 
 
-        // const {error} = await sendOtp(emailValue);
+            const {error} = await sendOtp(emailValue);
 
-        // if (error) {
-        //     setEmailError(error.message); 
-        //     return; 
-        // }
+            if (error) {
+                if (error.message === "Signups not allowed for otp") {
+                    setEmailError(''); 
+                } else {
+                    setEmailError(error.message); 
+                }
+
+                return; 
+            }
+
+        } else {
+            const oneTimeCode = otp.join(''); 
+
+            const {error} = await verifyOtp(emailValue, oneTimeCode); 
+
+            if (error) {
+                setEmailError(error.message); 
+                return; 
+            
+            }
+        }
+    }
+
+    const handleChange = (e, index) => {
+        const { value } = e.target; 
+
+        if (value.match(/^\d$/)) {
+            const newOtp = [...otp]; 
+            newOtp[index] = value; 
+            setOtp(newOtp); 
+
+            if (index < length - 1) {
+                inputs.current[index + 1].focus(); 
+            }
+        }; 
+    }
+
+    const handleKeyDown = (e, index) => {
+        if (e.key === 'Backspace') {
+            const newOtp = [...otp]; 
+            newOtp[index] = ''; 
+            setOtp(newOtp); 
+
+            if (index > 0) {
+                inputs.current[index - 1].focus(); 
+            }
+        }
+    }; 
+
+    function hasBlankValues(arr) {
+        return arr.some(element => element === "" || element === null || typeof element === 'undefined'); 
+    }
+
+    function hasNoBlankValues(arr) {
+        return arr.some(element => element !== "" && element !== null && typeof element !== 'undefined'); 
     }
 
     return (
        <div className="form-container">
             <h1 className="login-header">Welcome <span>back.</span></h1>
             <p className="input-header">Sign in to your account.</p>
-            <input data-testid="email-input" type="email" name="" id="" placeholder="Email" className="input-field" value={emailValue} onChange={handleEmailChange}/>
-            <div id="otp-container" className="otp-container hidden">
-
+            <input data-testid="email-input" disabled={hasNoBlankValues(otp) ? true : false} type="email" name="" id="" placeholder="Email" className="input-field" value={emailValue} onChange={handleEmailChange}/>
+            <div data-testid="otp-container" id="otp-container" className="otp-container hidden">
+                <div className="inputs-container">
+                    {otp.map((_, index) => (
+                        <input
+                            key={index}
+                            data-testid={`otp-input-${index}`}
+                            type="text"
+                            maxLength="1"
+                            value={otp[index]}
+                            onChange={(e) => handleChange(e, index)}
+                            onKeyDown={(e) => handleKeyDown(e, index)}
+                            ref={(el) => (inputs.current[index] = el)}
+                            style={{
+                                width: '40px',
+                                height: '40px',
+                                margin: '0 5px',
+                                textAlign: 'center',
+                                fontSize: '18px',
+                                border: '1px solid #ccc',
+                                borderRadius: '4px',
+                                background: "#242424"
+                            }}
+                        />
+                    ))}
+                </div>
+                <div className="choice-container">
+                    <button>Resend Code?</button>
+                </div>
             </div>
-            <button data-testid="submit-button" className="confirm-btn" id="signin-btn" onClick={handleSubmitEmail} disabled={isButtonDisabled}>Send Code</button>
+            <button data-testid="submit-button" className="confirm-btn" id="signin-btn" onClick={handleSubmitEmail} disabled={isButtonDisabled}>{submitButtonContent}</button>
             <div className="error-container">
                 <p data-testid="error-message" className="error-msg">{emailError}</p>
             </div>
